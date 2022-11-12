8c9f4523859e7f88b72c71469af9ecc32675f8da
Aaron Wilson <Aarjwilson@gmail.com>
2022-11-12 10:56:04 -0700

    updated classes (partial) an changed Plantuml name

--- game/director.py
+++ game/director.py
@@ -3,3 +3,17 @@ from game.card import Card
 class Director:
     def __init__(self):
         self.is_playing = True
+        self.score = 0
+        self.total_score = 300
+
+        card = Card()
+
+    def start_game(self):
+        while self.is_playing:
+            self.get_inputs()
+            self.do_updates()
+            self.do_outputs()
+
+    def get_inputs(self):
+        draw_card = input("Higher or Lower? [h/l] ")
+        
