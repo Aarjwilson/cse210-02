226b79449e8ead68beb18f6bbe00e7ce0cc241f3
Aaron Wilson <Aarjwilson@gmail.com>
2022-11-12 17:16:05 -0700

    updated class, method, and value names

--- game/card.py
+++ game/card.py
@@ -5,6 +5,6 @@ class Card:
     def __init__(self): #Set Default values
         self.value = 0
 
-    def draw_card(self): #Draw a random card number between 1 and 13
+    def DrawCard(self): #Draw a random card number between 1 and 13
         self.value = random.randrange(1,14)
 
--- game/director.py
+++ game/director.py
@@ -5,82 +5,83 @@ class Director:
         """
         Initialize the default values and create the two card objects.
         """
-        self.is_playing = True
+        self.isPlaying = True
         self.hiLo = ""
         self.score = 300 # Player starts out with 300 points.
 
-        self.current_card = Card()
-        self.next_card = Card()
+        self.currentCard = Card()
+        self.nextCard = Card()
 
-    def start_game(self):
+    def StartGame(self):
         """
         This will make for a one shot start of the game on main.py
 
         this is where we will get the starting card number and, while
         game is still running, where all the functions will be called.
         """
-        self.current_card.draw_card()
+        self.currentCard.DrawCard()
 
-        while self.is_playing:
-            self.get_inputs()
-            self.do_updates()
-            self.do_outputs()
-            self.play_again()
+        while self.isPlaying:
+            self.GetInputs()
+            self.DoUpdates()
+            self.DoOutputs()
+            self.PlayAgain()
             
 
-    def get_inputs(self):
+    def GetInputs(self):
         """
         Here we will display the first card and ask the player if it is Higher or lower.
         """
-        print(f"\nThe card is: {self.current_card.value}")
+        print(f"\nThe card is: {self.currentCard.value}")
+        
         self.hiLo = input("Higher or Lower? [h/l] ")
 
-    def do_updates(self):
+    def DoUpdates(self):
         """
         Here we will generate the next card number and compare it to the players guess.
         points addition or subtraction will be determined by the results
 
         if score reachs "0" game over will be generated.
         """
-        self.next_card.draw_card()
+        self.nextCard.DrawCard()
 
-        if self.hiLo.lower() == "h" and self.next_card.value > self.current_card.value: #Correctly Guessed High
+        if self.hiLo.lower() == "h" and self.nextCard.value > self.currentCard.value: #Correctly Guessed High
             self.score += 100
-        elif self.hiLo.lower() == "l" and self.next_card.value < self.current_card.value: #Correctly Guessed Low
+        elif self.hiLo.lower() == "l" and self.nextCard.value < self.currentCard.value: #Correctly Guessed Low
             self.score += 100
-        elif self.hiLo.lower() == "h" and self.next_card.value < self.current_card.value: #Incorrectly Guessed High
+        elif self.hiLo.lower() == "h" and self.nextCard.value < self.currentCard.value: #Incorrectly Guessed High
             self.score -= 75
-        elif self.hiLo.lower() == "l" and self.next_card.value > self.current_card.value: #Incorrectly Guessed Low
+        elif self.hiLo.lower() == "l" and self.nextCard.value > self.currentCard.value: #Incorrectly Guessed Low
             self.score -= 75
         #Seperate if statement to determine if game should be stopped sooner.
         if self.score <= 0:
-            self.is_playing = False
+            self.isPlaying = False
 
-    def do_outputs(self):
+    def DoOutputs(self):
         """
         Here we will print off the response, including the game over statement if 
         score reaches 0
         """
-        print(f"Next card was: {self.next_card.value}")
+        print(f"Next card was: {self.nextCard.value}")
         print(f"Your score is: {self.score}")
 
         if self.score <= 0:
             print("\nGame Over!\n")
 
-    def play_again(self):
+    def PlayAgain(self):
         """
         If not Game Over then the player can decide to play again or not.
 
         If game continues then the next card value will now be the currently cards value for the next round.
         """
-        if self.is_playing:
+        if self.isPlaying:
             play = input("Play again? [y/n] ")
 
             if play.lower() == "y":
-                self.current_card.value = self.next_card.value
-                self.next_card = Card() #set next card to default
+                self.currentCard.value = self.nextCard.value
+                self.nextCard = Card() #set next card to default
             elif play.lower() == "n":
-                self.is_playing = False
+                self.isPlaying = False
         
         else:
             print("Thank you for playing!")
